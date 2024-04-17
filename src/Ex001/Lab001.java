1939b4de7becf8d7d9e25827ba961176efdfefa7
NehaJagwani <166098621+NehaJagwani@users.noreply.github.com>
2024-04-17 15:29:02 +1000

    Update Lab001.java

--- src/Ex001/Lab001.java
+++ src/Ex001/Lab001.java
@@ -4,5 +4,6 @@ public class Lab001 {
     public static void main(String[] args) {
         System.out.println("My age is 65");
         System.out.println("checking again");
+        System.out.println("git pull");
     }
 }
